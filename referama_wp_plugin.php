1ef8e127550b4fd7fe6cfbaef3473e289b2692d7
Alejandro Riera <ariera@gmail.com>
2012-04-17 17:28:54 +0200

    update of the embed code

--- referama_wp_plugin.php
+++ referama_wp_plugin.php
@@ -26,15 +26,17 @@ function referama_embed($args){
   $html ="";
   if ($collection_id != 0){
     $html = <<<EOD
-      <div class="referama_anchor" data-rfm-id="$collection_id" data-rfm-visualization="$visualization"></div>
+      <div class="referama_anchor" data-rfm-collection-id="$collection_id" data-rfm-visualization="$visualization"></div>
+      
       <style type="text/css">
         .powered_by_referama .rfm_framed_edit{display:none;margin-left:5px;background-color:#eee;border-color:#f2f2f2;padding:3px 5px;color:#ccc;text-decoration:none;}
         .powered_by_referama:hover .rfm_framed_edit{display:inline;}
         .powered_by_referama .rfm_framed_edit:hover{color:#fff;background-color:blue;}
       </style>
       <div class="powered_by_referama">
-        Powered by <a href="http://www.referama.com" target="_blank">Referama</a> <a href="#" class="rfm_framed_edit" data-rfm-id="1">edit</a>
+        Powered by <a href="http://www.referama.com" target="_blank">Referama</a> <a href="#" class="rfm_framed_edit" data-rfm-collection-id="11">edit</a>
       </div>
+    
       <script type='text/javascript'>
         (function() {
             Referama = {};
@@ -44,6 +46,9 @@ function referama_embed($args){
             (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(rfm);
         })();
       </script>
+
+
+
 
 
 EOD;
