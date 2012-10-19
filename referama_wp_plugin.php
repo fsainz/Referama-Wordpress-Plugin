f5cc21043fe92b77f77d4f64f93067646334d248
Alejandro Riera <ariera@gmail.com>
2012-10-19 19:00:36 +0200

    arreglado el bug q no permitia embebed visualizaciones

--- referama_wp_plugin.php
+++ referama_wp_plugin.php
@@ -26,32 +26,20 @@ function referama_embed($args){
   $html ="";
   if ($collection_id != 0){
     $html = <<<EOD
-      <div class="referama_anchor" data-rfm-collection-id="$collection_id" data-rfm-visualization="$visualization"></div>
-      
-      <style type="text/css">
-        .powered_by_referama .rfm_framed_edit{display:none;margin-left:5px;background-color:#eee;border-color:#f2f2f2;padding:3px 5px;color:#ccc;text-decoration:none;}
-        .powered_by_referama:hover .rfm_framed_edit{display:inline;}
-        .powered_by_referama .rfm_framed_edit:hover{color:#fff;background-color:blue;}
-      </style>
-      <div class="powered_by_referama">
-        Powered by <a href="http://www.referama.com" target="_blank">Referama</a> <a href="#" class="rfm_framed_edit" data-rfm-collection-id="11">edit</a>
-      </div>
-    
+      <div class="referama_anchor" data-rfm-id="$collection_id" data-rfm-visualization="$visualization"></div>
       <script type='text/javascript'>
         (function() {
             Referama = {};
-            Referama.host = "$referama_host";
+            Referama.host = "//localhost/";
             var rfm = document.createElement('script'); rfm.type = 'text/javascript'; rfm.async = true;
             rfm.src = Referama.host + "javascripts/rfm_loader.js";
             (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(rfm);
         })();
       </script>
-
-
-
-
-
 EOD;
+    if ($visualization != 'widget'){
+      $html = $html."<div class='powered_by_referama'> Powered by <a href='http://www.referama.com' target='_blank'>Referama</a> </div>";
+    }
   }
   return $html;
 }
